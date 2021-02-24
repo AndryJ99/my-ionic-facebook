0db37016a669014d69bf698b3379883590628d08
andrea.serafin <serafin.andrea1999@gmail.com>
2021-02-24 15:06:05 +0100

    Partial Home page

--- src/app/groups/groups.page.ts
+++ src/app/groups/groups.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Post} from '../class/post';
-
 @Component({
   selector: 'app-groups',
   templateUrl: './groups.page.html',
--- src/app/my-post/my-post.component.ts
+++ src/app/my-post/my-post.component.ts
@@ -7,6 +7,7 @@ import {Post} from '../class/post';
   styleUrls: ['./my-post.component.scss'],
 })
 export class MyPostComponent implements OnInit {
+  expand = false;
   datas: Post[] = [
     {
       title: 'Esempio 1',
@@ -18,23 +19,25 @@ export class MyPostComponent implements OnInit {
       title: 'Esempio 2',
       subtitle: 'Sottotitolo 2',
       image: '../../assets/GroupPage/placeholder1.png',
-      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged'
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
     },
     {
       title: 'Esempio 3',
       subtitle: 'Sottotitolo 3',
       image: '../../assets/GroupPage/placeholder1.png',
-      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged'
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
     },
     {
       title: 'Esempio 4',
       subtitle: 'Sottotitolo 4',
       image: '../../assets/GroupPage/placeholder1.png',
-      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged'
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s'
     },
   ];
   constructor() { }
 
   ngOnInit() {}
-
+  clicked(){
+    this.expand = !this.expand;
+  }
 }
